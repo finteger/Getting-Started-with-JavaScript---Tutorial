46d4610153a04bde3576837c520bac0d5de6e222
finteger <nashassociatesinc@gmail.com>
2024-06-03 15:29:17 -0600

    my commit

---
+++ tny_script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Todd Nash    
+   Date: 2004-06-03
+
+*/
+
+//Display the current date & time
+document.getElementById("dateNow").innerHTML = "m/d/y<br />h:m:s";
+
+//Display the time left until New Year's End
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hr";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
