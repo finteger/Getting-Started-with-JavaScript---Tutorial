e527d0d6dca26104a1a24bfaf63cfd8dd6e0ae94
finteger <nashassociatesinc@gmail.com>
2024-06-04 14:30:58 -0600

    commit

--- tny_script.js
+++ tny_script.js
@@ -19,3 +19,31 @@ document.getElementById("days").textContent = "dd";
 document.getElementById("hrs").textContent = "hr";
 document.getElementById("mins").textContent = "mm";
 document.getElementById("secs").textContent = "ss";
+
+//New variables for date-time
+var currentDay = new Date("May 23, 2018 14:35:05");
+var dateString = currentDay.toLocaleDateString();
+var timeString = currentDay.toLocaleTimeString();
+
+
+/*
+This is a  
+multi
+line
+comment
+*/
+
+//We are dynamically setting Current Time
+document.getElementById("dateNow").innerHTML = 
+dateString + "<br />" + timeString;
+
+var newYear = new Date("January 1, 2018");
+
+var nextYear = currentDay.getFullYear() + 1;
+
+newYear.setFullYear(nextYear);
+
+
+console.log(nextYear);
+
+console.log(newYear);
