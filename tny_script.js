0de6c3d2bf9e0d5145a34949573c4f6288162eb4
finteger <nashassociatesinc@gmail.com>
2024-06-05 13:46:54 -0600

    commit

--- tny_script.js
+++ tny_script.js
@@ -20,46 +20,69 @@ document.getElementById("hrs").textContent = "hr";
 document.getElementById("mins").textContent = "mm";
 document.getElementById("secs").textContent = "ss";
 
-//New variables for date-time
-var currentDay = new Date();
-var dateString = currentDay.toLocaleDateString();
-var timeString = currentDay.toLocaleTimeString();
+
+function runClock(){
+
+   //New variables for date-time
+   var currentDay = new Date();
+   var dateString = currentDay.toLocaleDateString();
+   var timeString = currentDay.toLocaleTimeString();
 
 
-/*
-This is a  
-multi
-line
-comment
-*/
+   /*
+   This is a  
+   multi
+   line
+   comment
+   */
 
-//We are dynamically setting Current Time
-document.getElementById("dateNow").innerHTML = 
-dateString + "<br />" + timeString;
+   //We are dynamically setting Current Time
+   document.getElementById("dateNow").innerHTML = 
+   dateString + "<br />" + timeString;
 
 
-//Calculate the days until January 1st
-var newYear = new Date("January 1, 2018");
-var nextYear = currentDay.getFullYear() + 1;
-newYear.setFullYear(nextYear);
-var daysLeft = (newYear - currentDay) / (1000*60*60*24);
+   //Calculate the days until January 1st
+   var newYear = new Date("January 1, 2018");
+   var nextYear = currentDay.getFullYear() + 1;
+   newYear.setFullYear(nextYear);
+   var daysLeft = (newYear - currentDay) / (1000*60*60*24);
 
-var days = document.getElementById("days");
+   var days = document.getElementById("days");
 
-//Display the time left until New Year's Eve
+   //Display the time left until New Year's Eve
 
-//days
-days.innerHTML = daysLeft;
-days.textContent = Math.floor(daysLeft);
+   //days
+   days.innerHTML = daysLeft;
+   days.textContent = Math.floor(daysLeft);
 
-//hours
-var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
-document.getElementById("hrs").textContent = Math.floor(hrsLeft);
+   //hours
+   var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
+   document.getElementById("hrs").textContent = Math.floor(hrsLeft);
 
-//minutes
-var minsLeft = (hrsLeft  - Math.floor(hrsLeft))*60;
-document.getElementById("mins").textContent = Math.floor(minsLeft);
+   //minutes
+   var minsLeft = (hrsLeft  - Math.floor(hrsLeft))*60;
+   document.getElementById("mins").textContent = Math.floor(minsLeft);
 
-//seconds
-var secsLeft = (minsLeft  - Math.floor(minsLeft))*60;
-document.getElementById("secs").textContent = Math.floor(secsLeft);
+   //seconds
+   var secsLeft = (minsLeft  - Math.floor(minsLeft))*60;
+   document.getElementById("secs").textContent = Math.floor(secsLeft);
+
+}
+
+
+var watch = setInterval(() =>{
+   runClock();
+}, 1000);
+
+runClock();
+
+var stopButton = document.getElementById("stop");
+
+stopButton.addEventListener("click", () =>{
+      clearInterval(watch);
+});
+
+
+
+
+
